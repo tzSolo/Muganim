c68fc49677561206c6acafc027a18817eb036a65
tziviSolo <t0504114734@gmail.com>
2025-03-18 18:40:12 +0200

    Enable access for everyone by setting cors

--- Server/Server.API/Program.cs
+++ Server/Server.API/Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(opt => opt.AddPolicy("myPolicy", policy =>
+{
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+}));
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -12,14 +16,17 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 //if (app.Environment.IsDevelopment())
 //{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseHttpsRedirection();
+
+app.UseCors("myPolicy");
 
 app.UseAuthorization();
 
