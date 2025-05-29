2c70127147b10aa7dd1a680e885d42c0493b79c4
tzSolo <t0504114734@gmail.com>
2025-05-29 15:21:20 +0300

    Reading content file by Blob.text() function.

--- Client/Users/src/componnents/upload-file.tsx
+++ Client/Users/src/componnents/upload-file.tsx
@@ -1,18 +1,22 @@
-import { useContext, useState } from "react";
-import { readFile } from "./read-from-file";
+import { useContext, useEffect, useState } from "react";
 import { apiContext } from "../contexts/api-context";
 import { userContext } from "../contexts/user-context";
+import { UserPost } from "../models/user.post";
 
 
 const UploadFileToAWS = () => {
+    const [allUsers, setAllUsers] = useState<UserPost[]>([]);
+    const [email, setEmail] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
     const [files, setFiles] = useState<FileList | null>(null);
+    const [sharedWith, setSharedWith] = useState<UserPost[]>([]);
     const { url } = useContext(apiContext);
     const { userState } = useContext(userContext);
 
     const uploadFile = async () => {
         if (files) {
             const name = files[0].name;
-            const content = readFile(files[0]);
+            const content = await files[0].text();
 
             try {
                 const response = await fetch(`${url}/api/Files`, {
@@ -21,9 +25,11 @@ const UploadFileToAWS = () => {
                         'Content-Type': 'application/json',
                         'Authorization': `Bearer ${userState.token}`
                     },
-                    body: JSON.stringify({ name, content, sharedWith: [] })
+                    body: JSON.stringify({ name, content, sharedWith })
                 });
-
+                if (!response.ok) {
+                    throw new Error(`Error, status code : ${response.status}`);
+                }
                 const result = await response.json();
                 return result;
             }
@@ -31,10 +37,57 @@ const UploadFileToAWS = () => {
                 console.error(error);
             }
         }
-    }
+    };
+
+    const getAllUsersAsync = async () => {
+        try {
+            const response = await fetch(`${url}/api/Users`);
+            if (!response.ok) {
+                throw new Error(`Error ,status code : ${response.status}.`);
+            }
+            const data = await response.json();
+            setAllUsers(data);
+        }
+        catch (error: any) {
+            console.error(error);
+        }
+    };
+
+    const handleAddUser = () => {
+        const existUser = allUsers.find(u => u.email === email);
+
+        if (existUser) {
+            setSharedWith([...sharedWith, existUser]);
+            setEmail("");
+            setMessage("User added successfully.");
+        }
+        else {
+            setMessage("Email isn't valid.");
+        }
+    };
+
+    useEffect(() => {
+        const getAllUsers = async () => {
+            await getAllUsersAsync();
+        }
+
+        getAllUsers();
+    }, []);
 
     return <>
-        <input type="file" onChange={({ target }) => setFiles(target.files)} />
+        <input
+            type="file"
+            onChange={({ target }) => setFiles(target.files)}
+        />
+        <input
+            type="email"
+            value={email}
+            placeholder="Enter email to share"
+            onChange={({ target }) => setEmail(target.value)}
+        />
+        <button onClick={handleAddUser}>Add User</button>
+        <small>{message}</small>
+
         <button onClick={uploadFile}>Upload File</button>
     </>
 }
